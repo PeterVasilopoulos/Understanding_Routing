381b89dd94a19be8d2acec459654359285d5f4b7
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-06 15:29:58 -0400

    Assignment completed

--- server.py
+++ server.py
@@ -9,5 +9,36 @@ def hello_world():
 def dojo():
     return "Dojo!"
 
+@app.route('/say/<name>')
+def say_hi(name):
+    if isinstance(name, str):
+        return f"Hello {name}!"
+    else:
+        return "Please provide a name"
+
+@app.route('/repeat/<num>/<word>')
+def repeat(num, word):
+    str = ""
+    try:  
+        num = int(num)
+    except:
+        return "Please provide a number and a word"
+
+    # if isinstance(num, int) and isinstance(word, str):
+    #     for i in range(int(num)):
+    #         str += f"{word}\n"
+    #     return str
+    # else:
+    #     return "Please provide correct values"
+
+    for i in range(num):
+        str += f"{word}\n"
+    return str
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Sorry! No response. Try again."
+
+
 if __name__ == "__main__":
     app.run(debug = True)
